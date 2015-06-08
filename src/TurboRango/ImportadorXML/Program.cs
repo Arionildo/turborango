19ac2569ef59740f1364f8ddf5a21d3e7997c490
Arionildo <arionildo6@gmail.com>
2015-06-08 15:40:02 -0300

    Atualização05 TurboRango(Adição de métodos no Restaurante / Deleção do arquivo sln duplicado / Criação TurboRango.Web)

--- src/TurboRango/ImportadorXML/Program.cs
+++ src/TurboRango/ImportadorXML/Program.cs
@@ -42,3 +42,76 @@ namespace ImportadorXML
         }
     }
 }
+
+
+/* ESTUDAR CÓDIGO
+
+var ex1e = restaurantesXML.ApenasComUmRestaurante();
+            var ex1f = restaurantesXML.ApenasMaisPopulares();
+            var ex1g = restaurantesXML.BairrosComMenosPizzarias();
+            var ex1h = restaurantesXML.AgrupadosPorBairroPercentual();
+
+            var todos = restaurantesXML.TodosRestaurantes();
+
+            #endregion
+
+            #region ADO.NET
+
+            var connString = @"Data Source=.;Initial Catalog=TurboRango_dev;Integrated Security=True;";
+
+            var acessoAoBanco = new CarinhaQueManipulaOBanco(connString);
+
+            acessoAoBanco.Inserir(new Contato
+            {
+                 Site = "www.dogao.gif",
+                 Telefone = "5555555"
+            });
+
+            IEnumerable<Contato> contatos = acessoAoBanco.GetContatos();
+
+            var restaurantes = new Restaurantes(connString);
+
+            var tiririca = new Restaurante
+            {
+                Nome = "Tiririca",
+                Capacidade = 50,
+                Categoria = Categoria.Fastfood,
+                Contato = new Contato
+                {
+                    Site = "http://github.com/tiririca",
+                    Telefone = "5555 5555"
+                },
+                Localizacao = new Localizacao
+                {
+                    Bairro = "Vila Nova",
+                    Logradouro = "ERS 239, 2755",
+                    Latitude = -29.6646122,
+                    Longitude = -51.1188255
+                }
+            };
+
+            restaurantes.Inserir(tiririca);
+
+            foreach (var r in todos)
+            {
+                restaurantes.Inserir(r);
+            }
+
+            var todosBD = restaurantes.Todos();
+
+            // Atualizar dados do restaurante...
+
+            tiririca.Capacidade = 100;
+            tiririca.Nome = "Novo Tiririca Grill";
+            tiririca.Categoria = Categoria.Churrascaria;
+            tiririca.Contato.Site = "http://www.tiriricagrill.com.br";
+            tiririca.Contato.Telefone = "5544445555";
+            tiririca.Localizacao.Bairro = "Centro";
+            tiririca.Localizacao.Logradouro = "Avenida central";
+            tiririca.Localizacao.Latitude = -29.6646122;
+            tiririca.Localizacao.Longitude = -51.1188255;
+
+            restaurantes.Atualizar(375, tiririca);
+
+            #endregion
+*/
--- src/TurboRango/ImportadorXML/RestaurantesXML.cs
+++ src/TurboRango/ImportadorXML/RestaurantesXML.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using ConsoleApplication1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
-using TurboRango.Dominio;
 
 namespace ImportadorXML
 {
@@ -84,5 +84,94 @@ namespace ImportadorXML
                 })
                 .Select(r => r);
         }
+
+        public IList<Categoria> CategoriaComUmRestaurante()
+        {
+            return (IList<Categoria>) restaurantes
+                .GroupBy(x => x.Attribute("categoria").Value)
+                .Where(g => g.Count() == 1)
+                .Select(g => Enum.Parse(typeof(Categoria),g.Key)/*.ToEnum<Categoria>()*/)
+                .ToList();
+        }
     }
 }
+
+
+/* ESTUDAR CÓDIGO
+public IList<Categoria> ApenasComUmRestaurante()
+        {
+            return restaurantes.GroupBy(x => x.Attribute("categoria").Value).Where(g => g.Count() == 1).Select(g => g.Key.ToEnum<Categoria>() ).ToList();
+        }
+
+        public IList<Categoria> ApenasMaisPopulares()
+        {
+            //return restaurantes.GroupBy(n => n.Attribute("categoria").Value).Where(g => g.Count() > 2).OrderByDescending(g => g.Count()).Take(2).Select(g => g.Key.ToEnum<Categoria>() ).ToList();
+            return (
+                from n in restaurantes
+                group n by n.Attribute("categoria").Value into g
+                let groupLength = g.Count()
+                where groupLength > 2
+                orderby groupLength descending
+                select g.Key.ToEnum<Categoria>()
+            ).Take(2).ToList();
+        }
+
+        public IList<string> BairrosComMenosPizzarias()
+        {
+            //return restaurantes
+            //    .Where(n => n.Attribute("categoria").Value.ToEnum<Categoria>() == Categoria.Pizzaria)
+            //    .GroupBy(n => n.Element("localizacao").Element("bairro").Value)
+            //    .OrderBy(g => g.Count())
+            //    .Take(8)
+            //    .Select(g => g.Key)
+            //    .ToList();
+            return (
+                from n in restaurantes
+                let cat = n.Attribute("categoria").Value.ToEnum<Categoria>()
+                where cat == Categoria.Pizzaria
+                group n by n.Element("localizacao").Element("bairro").Value into g
+                orderby g.Count()
+                select g.Key
+            ).Take(8).ToList();
+        }
+
+        public object AgrupadosPorBairroPercentual()
+        {
+            //return restaurantes.GroupBy(n => n.Element("localizacao").Element("bairro").Value).Select(g => new { Bairro = g.Key, Percentual = Math.Round(Convert.ToDouble(g.Count() * 100) / restaurantes.Count(), 2) }).OrderByDescending(g => g.Percentual);
+            return (
+                from n in restaurantes
+                group n by n.Element("localizacao").Element("bairro").Value into g
+                let totalRestaurantes = restaurantes.Count()
+                select new { Bairro = g.Key, Percentual = Math.Round(Convert.ToDouble(g.Count() * 100) / totalRestaurantes, 2) }
+            ).OrderByDescending(g => g.Percentual);
+        }
+
+        public IEnumerable<Restaurante> TodosRestaurantes()
+        {
+            return (
+                from n in restaurantes
+                let contato = n.Element("contato")
+                let site = contato != null && contato.Element("site") != null ? contato.Element("site").Value : null
+                let telefone = contato != null && contato.Element("telefone") != null ? contato.Element("telefone").Value : null
+                let localizacao = n.Element("localizacao")
+                select new Restaurante
+                {
+                     Nome = n.Attribute("nome").Value,
+                     Capacidade = Convert.ToInt32(n.Attribute("capacidade").Value),
+                     Categoria = (Categoria)Enum.Parse(typeof(Categoria), n.Attribute("categoria").Value, ignoreCase: true),
+                     Contato = new Contato
+                     {
+                          Site = site,
+                          Telefone = telefone
+                     },
+                     Localizacao = new Localizacao
+                     {
+                          Bairro = localizacao.Element("bairro").Value,
+                          Logradouro = localizacao.Element("logradouro").Value,
+                          Latitude = Convert.ToDouble(localizacao.Element("latitude").Value),
+                          Longitude = Convert.ToDouble(localizacao.Element("longitude").Value)
+                     }
+                }
+            );
+        }
+*/
