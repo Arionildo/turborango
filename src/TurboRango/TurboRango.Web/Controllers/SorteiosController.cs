db542202a518ec2d7ceb2c35fa954b2078116c6a
Arionildo <arionildo6@gmail.com>
2015-06-10 16:58:16 -0300

    TurboRango Versão 2(Criação de sorteio de restaurantes mais próximos)

---
+++ src/TurboRango/TurboRango.Web/Controllers/SorteiosController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using TurboRango.Web.Models;
+using System.Linq;
+
+namespace TurboRango.Web
+{
+    public class SorteiosController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Sorteios
+        public ActionResult Index()
+        {
+            ViewBag.QtdRestaurantes = db.Restaurantes.Count();
+            return View();
+        }
+    }
+}
